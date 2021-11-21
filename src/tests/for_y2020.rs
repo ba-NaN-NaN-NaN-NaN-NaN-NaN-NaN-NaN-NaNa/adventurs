a373553df04239802ab2f0b823977ac7d8009e47
Erik Mossberg <fruityloops@klantig.se>
2021-11-21 17:06:37 +0000

    Implement day 1 calculations from disk-stored data.

--- src/tests/for_y2020.rs
+++ src/tests/for_y2020.rs
@@ -13,6 +13,9 @@ mod y2020tests {
 
     use crate::y2020::d1;
     use crate::structs::I64Pair;
+    use crate::input;
+    use std::path::Path;
+
     #[test]
     fn d1_additive_complement() {
         let d1set = d1::i64_set_builder(D1_SAMPLE);
@@ -24,6 +27,30 @@ mod y2020tests {
                 assert!(x == 299);
             },
             None    => assert!(false, "Did not find summable pairs."),
+        }
+
+        let fname_abs = match input::get_input("2020_d1.txt") {
+            Err(why) => {
+                assert!(false, "Could not open skarp input: {}", why);
+                std::path::PathBuf::new()
+            },
+            Ok(s) => {
+                s
+            }
+        };
+
+        match input::readstring(&fname_abs) {
+            Err(why) => assert!(false, "Could not open skarp input: {}", why),
+            Ok(s) => {
+                let skarp = d1::i64_set_builder(s.as_str());
+                match d1::find_summable_pairs(&skarp){
+                    Some(I64Pair{x,y}) => {
+                        println!("Found summable pairs: {} * {} -> {}\n", x, y, x*y);
+                        assert!(x == 277);
+                    },
+                    None    => assert!(false, "Did not find summable pairs."),
+                }
+            }
         }
     }
 }
