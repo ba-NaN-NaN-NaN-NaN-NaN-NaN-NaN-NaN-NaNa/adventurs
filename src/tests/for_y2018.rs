876229658763a04e56a77ff737cb60f24c8a6bef
Erik Mossberg <fruityloops@klantig.se>
2021-11-21 13:46:24 +0000

    Impement reaction reducer.

--- src/tests/for_y2018.rs
+++ src/tests/for_y2018.rs
@@ -1,12 +1,26 @@
 
 
 #[cfg(test)]
-mod tests {
+mod y2018tests {
     use crate::y2018::d1::freq_summer;
     #[test]
-    fn it_works() {
+    fn d1_frequency_summer() {
         assert_eq!(freq_summer(0, "+1, +1, +1"),  3);
         assert_eq!(freq_summer(0, "+1, +1, -2 "),  0);
         assert_eq!(freq_summer(0, "-1, -2, -3 "), -6);
+    }
+
+    use crate::y2018::d5::reducer;
+
+    #[test]
+    fn d5_reducer() {
+        assert_eq!(reducer("ab".as_bytes().to_vec()), "ab".as_bytes().to_vec());
+        assert_eq!(reducer("aa".as_bytes().to_vec()), "aa".as_bytes().to_vec());
+        assert_eq!(reducer("aA".as_bytes().to_vec()), "".as_bytes().to_vec());
+
+        assert_eq!(reducer("dabAcCaCBAcCcaDA".as_bytes().to_vec()), "dabAaCBAcCcaDA".as_bytes().to_vec());
+        assert_eq!(reducer("dabAaCBAcCcaDA".as_bytes().to_vec()), "dabCBAcCcaDA".as_bytes().to_vec());
+        assert_eq!(reducer("dabCBAcCcaDA".as_bytes().to_vec()), "dabCBAcaDA".as_bytes().to_vec());
+        assert_eq!(reducer("dabCBAcaDA".as_bytes().to_vec()), "dabCBAcaDA".as_bytes().to_vec());
     }
 }
---
+++ src/y2018/d5.rs
@@ -0,0 +1,34 @@
+
+
+pub fn reducer(s : Vec<u8>) -> Vec<u8> {
+    // Reduce first pair of 'xX' or 'Xx' where x can be any char, but x and X must be different cases.
+
+    if s.len() < 1 {
+        return s.to_vec()
+    }   
+    
+    for n in  0..(s.len() -1) {
+        // print!("fdh {}", n);
+        if s[n] != s[n+1] {
+            let c1 = char::from(s[n]);
+            let c2 = char::from(s[n+1]);
+
+            let mut c1l = char::from(s[n]);
+            c1l.make_ascii_lowercase();
+            let mut c2l = char::from(s[n+1]);
+            c2l.make_ascii_lowercase();
+
+            if c1 != c2 && c1l == c2l  {
+                //return s[0..n] + s[n+2..]
+                // print!("REDUCING of {} and {} / {} and {}\n", c1, c2, c1l, c2l);
+                let mut v = Vec::new();
+                v.extend(s[0..n].iter().cloned());
+                v.extend(s[n+2..].iter().cloned());
+                return v
+            }
+            // print!("No reduction of {} and {} / {} and {}\n", c1, c2, c1l, c2l);
+        }
+    }
+
+    return s.to_vec()
+}
