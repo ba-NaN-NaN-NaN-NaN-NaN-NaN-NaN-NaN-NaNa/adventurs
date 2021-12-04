377f5bb97229f3524b4a52a53dfd12d0e796b3bb
Erik Mossberg <fruityloops@klantig.se>
2021-12-04 13:29:24 +0000

    Initial implementation of a cardinal direction grid walker.

--- src/tests/for_y2020.rs
+++ src/tests/for_y2020.rs
@@ -64,4 +64,46 @@ mod y2020tests {
             }
         }
     }
+
+    use crate::grids::walker::{GridWalker, Walking};
+
+    #[test]
+    fn d12_gridwalker() {
+        let mut test_ship = GridWalker::new(0, 0);
+        let test_moves_1 = "\
+        F10
+";
+        let test_moves_2 = "\
+N3
+F7
+R90
+F11
+";
+
+        test_ship.ingest_moves(test_moves_1);
+        assert_eq!(test_ship.manhattan_dist_origin(),10);
+
+        test_ship.ingest_moves(test_moves_2);
+        assert_eq!(test_ship.manhattan_dist_origin(),25);
+
+        // Skarp
+        let fname_abs = match input::get_input("2020_d12.txt") {
+            Err(why) => {
+                assert!(false, "Could not open skarp input: {}", why);
+                std::path::PathBuf::new()
+            },
+            Ok(s) => {
+                s
+            }
+        };
+
+        match input::readstring(&fname_abs) {
+            Err(why) => assert!(false, "Could not open skarp input: {}", why),
+            Ok(s) => {
+                let mut skarp_ship = GridWalker::new(0, 0);
+                skarp_ship.ingest_moves(&s);
+                assert_eq!(skarp_ship.manhattan_dist_origin(), 1533);
+            }
+        }
+    }
 }
