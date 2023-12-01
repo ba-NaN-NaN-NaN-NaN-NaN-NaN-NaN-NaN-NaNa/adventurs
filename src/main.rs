c35021045e669f20cbbecb805c2592e8fdf3a819
Erik Mossberg <fruityloops@klantig.se>
2023-12-01 21:58:25 +0000

    Rust project structure fixes.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 mod tests;
 mod y2018;
+mod y2019;
 mod y2020;
 mod y2021;
+mod y2022;
+mod y2023;
 mod structs;
 mod input;
 mod grids;
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,3 +1,5 @@
 pub mod for_y2018;
 pub mod for_y2020;
 pub mod for_y2021;
+pub mod for_y2022;
+pub mod for_y2023;
