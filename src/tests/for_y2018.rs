207bb800e48094811c89a7796086708b536bfdca
Erik Mossberg <fruityloops@klantig.se>
2023-12-02 19:12:29 +0000

    (Re-)Implement 2018 d1.

--- src/tests/for_y2018.rs
+++ src/tests/for_y2018.rs
@@ -2,14 +2,6 @@
 
 #[cfg(test)]
 mod y2018tests {
-    use crate::y2018::d1::freq_summer;
-    #[test]
-    fn d1_frequency_summer() {
-        assert_eq!(freq_summer(0, "+1, +1, +1"),  3);
-        assert_eq!(freq_summer(0, "+1, +1, -2 "),  0);
-        assert_eq!(freq_summer(0, "-1, -2, -3 "), -6);
-    }
-
     use crate::y2018::d5::reducer;
 
     #[test]
--- src/y2018/d1.rs
+++ src/y2018/d1.rs
@@ -1,17 +1,72 @@
-use std::str::FromStr;
+use std::collections::HashSet;
 
 #[allow(dead_code)]
-pub fn freq_summer(origin: i32, deltas: &str) -> i32 {
-    let owned = String::from(deltas);
-    let parts = owned.split(",");
-    let mut acc = origin;
-    for part in parts {
-        //print!("Got a part {}", part);
-        let s = part.trim();
-        let my_int = i32::from_str(s).unwrap();
-        acc += my_int
+pub fn part1(input: &str) -> i64{
+    let lines:Vec<&str> = input.split("\n").collect();
+    let mut toreturn = 0;
+    for line in lines  {
+        let to_parse = line.trim();
+        if to_parse.len() == 0 {
+            continue;
+        }
+        // println!("Will parse {}.", to_parse);
+        let change = to_parse.parse::<i64>().unwrap();
+        toreturn += change;
     }
-    // print!("Parts is {}", parts);
-    // origin + parts.count() as i32
-    acc
+    return toreturn
+}
+
+#[allow(dead_code)]
+pub fn part2(input: &str) -> i64{
+    let lines:Vec<&str> = input.split("\n").collect();
+    let mut encountered: HashSet<i64> = HashSet::<i64>::new();
+    let mut current_freq = 0;
+    
+    for _ in 0..i64::MAX {
+        for line in lines.clone()  {
+            let to_parse = line.trim();
+            if to_parse.len() == 0 {
+                continue;
+            }
+            // println!("Will parse {}.", to_parse);
+            let change = to_parse.parse::<i64>().unwrap();
+            current_freq += change;
+            if encountered.contains(&current_freq) {
+                return current_freq
+            }
+            encountered.insert(current_freq);
+        }
+    }
+    //   return current_freq
+    return i64::MAX
+}
+
+
+#[cfg(test)]
+mod tests {
+    // Run these tests using:
+    // $ cargo test --package adventurs --bin adventurs -- y2018::d1::tests --nocapture
+
+    use super::*;
+    use crate::input;
+
+    #[test]
+    fn test_d1_full() {
+        {
+            assert_eq!(part1("+1 \n -2 \n +3 \n +1 \n "), 3);
+
+            assert_eq!(part1("+1\n +1\n -2"),   0);
+            assert_eq!(part1("-1\n -2\n -3"), -6);
+
+            let pbuf = input::get_input("2018_d1.txt").unwrap();
+            let content = input::readstring(&pbuf).unwrap();
+            assert_eq!(part1(content.as_str()), 538);
+        }
+        {
+            assert_eq!(part2("+1 \n -2 \n +3 \n +1 \n "), 2);
+            let pbuf = input::get_input("2018_d1.txt").unwrap();
+            let content = input::readstring(&pbuf).unwrap();
+            assert_eq!(part2(content.as_str()), 77271);
+        }
+    }
 }
